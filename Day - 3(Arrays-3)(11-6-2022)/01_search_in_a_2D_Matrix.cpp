35184c5cc7ad184ca0b519364df1db6dfcb61fb0
Baroor Avaneshwari <60316776+avani1403@users.noreply.github.com>
2022-06-11 11:46:16 +0530

    using Binary search

--- Day - 3(Arrays-3)(11-6-2022)/01_search_in_a_2D_Matrix.cpp
+++ Day - 3(Arrays-3)(11-6-2022)/01_search_in_a_2D_Matrix.cpp
@@ -1,1 +1,32 @@
 
+#include <bits/stdc++.h> 
+#include <vector>
+
+bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
+   //using brute force TC: O(m*n)
+    
+ /* for(int i = 0; i<m; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            if(mat[i][j] == target)
+                return true;
+        }
+    }
+    return false;
+    */
+    //using Binary Search
+    
+    int low = 0;
+    int high = m*n-1;
+    int col = n;
+    while(low <= high)
+    {
+        int mid = low+(high-low)/2;
+        int elem =mat[mid/col][mid%col];
+        if(elem == target) return true;
+        else if(elem < target) low = mid+1;
+        else high = mid-1;
+    }
+    return false;
+}
