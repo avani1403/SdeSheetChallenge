e15dda0fb71736bd8a13404b26de5b8dbaa1c5d1
Baroor Avaneshwari <60316776+avani1403@users.noreply.github.com>
2022-06-16 10:10:40 +0530

    intersection of Y LL

--- Day - 6(LinkedList - 2)(15-06-2022)/01_Intersection_of_2LL.cpp
+++ Day - 6(LinkedList - 2)(15-06-2022)/01_Intersection_of_2LL.cpp
@@ -1,1 +1,41 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
 
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+int findIntersection(Node *firstHead, Node *secondHead)
+{
+   if(firstHead == NULL || secondHead == NULL) return NULL;
+    Node* temp1 = firstHead;
+    Node* temp2 = secondHead;
+    while(temp1 != temp2)
+    {
+        if(temp1 == NULL)
+        {
+            temp1 = secondHead;
+        }else temp1 = temp1 -> next;
+        if(temp2 == NULL)
+        {
+            temp2 = firstHead;
+        }else temp2 = temp2 -> next;
+        
+    }
+    if(temp1 == NULL) return -1;
+    return temp1->data;
+    
+    
+}
