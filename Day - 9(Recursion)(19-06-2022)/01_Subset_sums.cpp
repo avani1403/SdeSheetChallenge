e43db672bf5d96a9481ca42076f15f3844b126d3
Baroor Avaneshwari <60316776+avani1403@users.noreply.github.com>
2022-06-19 16:04:40 +0530

    recursion

--- Day - 9(Recursion)(19-06-2022)/01_Subset_sums.cpp
+++ Day - 9(Recursion)(19-06-2022)/01_Subset_sums.cpp
@@ -1,1 +1,22 @@
+void solve(int i,int sum, vector<int> arr, vector<int> &ans)
+{
+    if(i >= arr.size())
+    {
+        ans.push_back(sum);
+        return;
+    }
+    
+    sum += arr[i];
+    solve(i+1, sum, arr, ans);
+    
+    sum -= arr[i];
+    solve(i+1, sum, arr, ans);
+}
 
+vector<int> subsetSum(vector<int> &num)
+{
+    vector<int> ans;
+    solve(0, 0, num, ans);
+    sort(ans.begin(), ans.end());
+    return ans;
+}
