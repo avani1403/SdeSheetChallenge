10fdab05914688dafd6045e602d96c2cf59e4ac3
Baroor Avaneshwari <60316776+avani1403@users.noreply.github.com>
2022-06-21 08:34:36 +0530

    Create 01_permutations_of_a_string.cpp

---
+++ Day : 10 (Recursion and Backtracking)/01_permutations_of_a_string.cpp
@@ -0,0 +1,24 @@
+void solve(int ind, string s, vector<string> &ans)
+{
+    if(ind >= s.size())
+    {
+        ans.push_back(s);
+        return;
+    }
+    
+    for(int i = ind; i<s.size(); i++)
+    {
+        swap(s[ind], s[i]);
+        solve(ind + 1, s, ans );
+        swap(s[ind], s[i]);
+    }
+}
+
+
+
+vector<string> findPermutations(string &s) {
+    // Write your code here.
+    vector<string> ans;
+    solve(0, s, ans);
+    return ans;
+}
