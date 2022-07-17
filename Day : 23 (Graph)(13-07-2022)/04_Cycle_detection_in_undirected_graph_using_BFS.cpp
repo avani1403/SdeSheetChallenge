9744ce4e360f6b97c74228a9a6c3a684d4e86983
Baroor Avaneshwari <60316776+avani1403@users.noreply.github.com>
2022-07-17 11:15:50 +0530

    Create 04_Cycle_detection_in_undirected_graph_using_BFS.cpp

---
+++ Day : 23 (Graph)(13-07-2022)/04_Cycle_detection_in_undirected_graph_using_BFS.cpp
@@ -0,0 +1,117 @@
+
+//Using BFS
+
+#include<bits/stdc++.h>
+
+bool isCycleBFS(int node, unordered_map<int, list<int>> &adj, unordered_map<int, bool> &visited){
+    
+    unordered_map<int, int> parent;
+    parent[node] = -1;
+    visited[node] = 1;
+    queue<int> q;
+    q.push(node);
+   
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+        for(auto neighbour: adj[front]){
+            if(visited[neighbour] == true && neighbour != parent[front]){
+                return true;
+            }
+            else if(!visited[neighbour]){
+                q.push(neighbour);
+                visited[neighbour] =1;
+                parent[neighbour] = front;
+            }
+        }
+    }
+    return false;
+}
+
+ 
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{
+    // Write your code here.
+//     adj List
+    unordered_map<int, list<int>> adj;
+   
+    for(int i=0;i<m; i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+       
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+   
+    unordered_map<int, bool> visited;
+    for(int i =0; i<n; i++){
+        if(!visited[i]){
+            bool ans = isCycleBFS(i, adj, visited);
+            if(ans == 1){
+                return "Yes";
+            }
+        }
+    }
+    return "No";
+}
+
+
+//USING DFS
+
+// bool isCyclic(vector<int> adj[],vector<int> &vis,int v,int parent){
+
+//     vis[v] =1;
+
+//     for(int child : adj[v]){
+
+//         if(!vis[child]){
+
+//             if(isCyclic(adj,vis,child,v)) return true;
+
+//         }
+
+//         else if(child != parent){
+
+//             return true;
+
+//         }
+
+//     }
+
+//     return false;
+
+// }
+
+// string cycleDetection (vector<vector<int>>& edges, int n, int m)
+
+// {
+
+//     // Write your code here.
+
+//     vector<int> adj[n+1];
+
+//     for(int i = 0; i<m; i++){
+
+//         adj[edges[i][0]].push_back(edges[i][1]);
+
+//         adj[edges[i][1]].push_back(edges[i][0]);
+
+//     }
+
+//     vector<int> vis(n+1,0);
+
+//     for(int i =1; i<=n; i++){
+
+//         if(!vis[i] && isCyclic(adj, vis, i, -1)){
+
+//             return "Yes";
+
+//         }
+
+//     }
+
+//     return "No";
+
+    
+
+// }
